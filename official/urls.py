65314ea3987b674a2cb36106076dfb4ed84c63c8
MUGISHA Emmanuel <you@example.com>
2023-05-17 18:05:31 +0300

    second commit

--- official/urls.py
+++ official/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-handler404 = 'official.views.handler404'
-handler500 = 'official.views.handler500'
+
 
 urlpatterns = [
     path("", views.home, name="wire24"),
--- official/views.py
+++ official/views.py
@@ -11,8 +11,3 @@ def faq(request):
 def terms(request):
     return render(request,"official/terms.html")
 
-def handler404(request, exception):
-    return render(request, 'errors/404.html', status=404)
-
-def handler500(request):
-    return render(request, 'errors/500.html', status=500)
