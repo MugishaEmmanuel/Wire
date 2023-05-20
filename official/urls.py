3f58e4bab5e5470c256432aa37778d97407f3a8b
MUGISHA Emmanuel <you@example.com>
2023-05-20 14:49:18 +0300

    first commit

--- official/urls.py
+++ official/urls.py
@@ -5,7 +5,4 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="wire24"),
-    path("faq/", views.faq, name="faq"),
-    path("termsandconditions/", views.terms, name="terms"),
-   
 ]
--- official/views.py
+++ official/views.py
@@ -5,9 +5,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, "official/home.html")
 
-def faq(request):
-    return render(request, "official/faq.html")
 
-def terms(request):
-    return render(request,"official/terms.html")
 
